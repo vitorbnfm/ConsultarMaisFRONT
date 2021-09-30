d6e7aae56f0ecb85aaa57d08518c7c53539d3192
Vitor Bonfim <50812235+vitorbnfm@users.noreply.github.com>
2021-09-30 14:10:14 -0300

    Models

---
+++ src/app/models/paciente.ts
@@ -0,0 +1,9 @@
+export interface Paciente {
+    id?: number;
+    nome: string;
+    datanascimento: string;
+    endereco: string;
+    celular: string;
+    email: string;
+    criadoem?: string;
+}
