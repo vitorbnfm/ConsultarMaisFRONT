6a1ce12fde2fa00c0386959d59c1c4b98f8efe39
Kamila Reis <34647680+kamilareis@users.noreply.github.com>
2021-09-30 23:59:25 -0300

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CadastrarPacienteComponent } from './components/views/paciente/cadastrar-paciente/cadastrar-paciente.component';
+import { ListarPacienteComponent } from './components/views/paciente/listar-paciente/listar-paciente.component';
+import { CadastrarMedicoComponent } from './components/views/medico/cadastrar-medico/cadastrar-medico.component';
+import { ListarMedicoComponent } from './components/views/medico/listar-medico/listar-medico.component';
+import { CadastrarConsultaComponent } from './components/views/consulta/cadastrar-consulta/cadastrar-consulta.component';
+import { ListarConsultaComponent } from './components/views/consulta/listar-consulta/listar-consulta.component';
+import { CadastrarUsuarioComponent } from './components/views/usuario/cadastrar-usuario/cadastrar-usuario.component';
+import { ListarUsuarioComponent } from './components/views/usuario/listar-usuario/listar-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastrarPacienteComponent,
+    ListarPacienteComponent,
+    CadastrarMedicoComponent,
+    ListarMedicoComponent,
+    CadastrarConsultaComponent,
+    ListarConsultaComponent,
+    CadastrarUsuarioComponent,
+    ListarUsuarioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
