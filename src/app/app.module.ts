7f69f6edd251fbaeaabb7329c3f317b03d8e63c8
Igor <igor.m99@hotmail.com>
2021-11-17 17:34:52 -0300

    Login funcionando, porém é um login através de um GET para o backend, não sendo algo tão seguro e tão bem montado ainda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -46,7 +46,8 @@ import { MatInputModule } from '@angular/material/input';
     MatIconModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/views/tela-login/tela-login.component.ts
+++ src/app/components/views/tela-login/tela-login.component.ts
@@ -1,4 +1,6 @@
+import { UsuarioService } from 'src/app/services/usuario.service';
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario';
 
 @Component({
   selector: 'app-tela-login',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TelaLoginComponent implements OnInit {
 
-  constructor() { }
+  nome!: string;
+  login!: string;
+  senha!: string;
+
+  constructor(private service: UsuarioService) { }
 
   ngOnInit(): void {
+  }
+
+  logar() : void {
+    let credenciais: Usuario = {
+      nome: this.nome,
+      login: this.login,
+      senha: this.senha,
+    }
+    
+    this.service.logar(credenciais).subscribe((credenciais) => {
+      console.log(credenciais);
+    })
   }
 
 }
