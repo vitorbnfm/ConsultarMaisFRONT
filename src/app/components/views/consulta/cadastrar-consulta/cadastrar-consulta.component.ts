07159d4c548d44fb556bfad1c148381a35fe8b4b
Igor <igor.m99@hotmail.com>
2021-11-26 11:58:37 -0300

    Consertando

--- src/app/components/views/consulta/cadastrar-consulta/cadastrar-consulta.component.ts
+++ src/app/components/views/consulta/cadastrar-consulta/cadastrar-consulta.component.ts
@@ -1,11 +1,9 @@
 import { MedicoService } from './../../../../services/medico.service';
-import { Usuario } from './../../../../models/usuario';
+import { PacienteService } from './../../../../services/paciente.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Consulta } from 'src/app/models/consulta';
 import { ConsultaService } from 'src/app/services/consulta.service';
-import { UsuarioService } from 'src/app/services/usuario.service';
-import { Medico } from 'src/app/models/medico';
 
 @Component({
   selector: 'app-cadastrar-consulta',
@@ -15,28 +13,17 @@ import { Medico } from 'src/app/models/medico';
 export class CadastrarConsultaComponent implements OnInit {
   dataconsulta!: string;
   medico!: string;
+  paciente!: string;
   medicoId!: number;
-  usuario!: string;
   usuarioId!: number;
   datanascimento!: string;
-  arrayMedicos: Medico[] = [];
 
-  constructor(private service: ConsultaService, private userService: UsuarioService, private medService: MedicoService, private router: Router) { }
+  constructor(private consultaService: ConsultaService, pacienteService: PacienteService, medicoService: MedicoService, private router: Router) { }
 
-  ngOnInit(): void {
-    let usuario = this.userService.listbyid().subscribe((a) => {
-      this.usuario = a.nome;
-      this.usuarioId = a.id!;
-    });
-
-    let medicos = this.medService.list().subscribe((a) => {
-      this.arrayMedicos = a;
-      console.log(this.arrayMedicos);
-    })
-  };
+  ngOnInit(): void { }
 
   cadastrar(): void {
-    
+
     let consulta: any = {
       dataconsulta: this.dataconsulta,
       medicoId: this.medicoId,
@@ -44,9 +31,9 @@ export class CadastrarConsultaComponent implements OnInit {
       datanascimento: this.datanascimento,
     };
 
-    this.service.create(consulta).subscribe((consulta) => {
+    this.consultaService.create(consulta).subscribe((consulta) => {
       console.log(consulta);
       this.router.navigate(["consulta/listar"]);
     });
-  }
+  };
 }
